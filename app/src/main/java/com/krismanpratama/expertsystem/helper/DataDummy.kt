591359e8332e8c5776d2fafe356e3f5d212282c0
krismandev <krismanpratama@gmail.com>
2021-06-11 13:48:10 +0700

    hasil perhitungan cf

--- app/src/main/java/com/krismanpratama/expertsystem/helper/DataDummy.kt
+++ app/src/main/java/com/krismanpratama/expertsystem/helper/DataDummy.kt
@@ -1,7 +1,33 @@
 package com.krismanpratama.expertsystem.helper
 
+import com.krismanpratama.expertsystem.data.entity.Gejala
+import com.krismanpratama.expertsystem.data.entity.Penyakit
+
 object DataDummy {
 
+    fun generateDummyPenyakit() : List<Penyakit>{
+        val listPenyakit = ArrayList<Penyakit>()
+        listPenyakit.add(
+            Penyakit(
+                1,
+                "Berak Kapur",
+                "Coba Cari di Google",
+                "Cari Di Google"
+            )
+        )
 
+        return listPenyakit
+    }
+
+    fun generateDummyGejala() : List<Gejala>{
+        val listGejala = ArrayList<Gejala>()
+        listGejala.add(
+            Gejala(
+                1,
+                "Nafsu Makan Berkurang",
+            )
+        )
+        return listGejala
+    }
 
 }
